17737ccfd0b1cae78a2e27634ea5606e1fa279b7
Valentin Vago <zeropaper@irata.ch>
2011-10-20 08:33:42 +0000

    small typo in jQuery package name

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+
 var Kern = require('./lib/kern').Kern;
 var kernApp = new Kern({
   contentDir:   __dirname +'/content',
   publicDir:    __dirname +'/public',
-  port:         8080,
-  hostname:     '0.0.0.0',
+  port:         process.env.C9_PORT,
+  hostname:     '0.0.0.0'
   /*
   session: {
     secret: 'we9dwed',
